19eaef12a943359306b8dd0d9fc392d14c4fee96
MohammedSF4109 <mohammed.sf4109@gmail.com>
2023-02-15 14:39:07 +0530

    added compile project files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GetEnvironmentalVariables
+{
+    class Program{
+        public static void Main(string[] args)
+        {
+             //calling method to create text file
+            //calling process method
+            string variableName = Console.ReadLine();
+            StartProcess(variableName);
+        }
+
+         static void StartProcess(string variableName)
+        {
+            //creating a process
+            //creating instance of process to execute the process
+            Process cmdStart = new Process();
+            cmdStart.StartInfo.FileName = "cmd.exe";
+            cmdStart.StartInfo.Arguments = $"/c echo %{variableName}%";
+            cmdStart.Start();
+        }
+
+    }
+}
